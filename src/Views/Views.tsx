160a6fbebbfe6611532d608befb4eb90cd92ad00
Yini Shi <yini@hume.ai>
2024-05-01 10:01:17 -0400

    update initial message and bump packages

--- src/Views/Views.tsx
+++ src/Views/Views.tsx
@@ -9,9 +9,12 @@ import { useVoice } from '@humeai/voice-react';
 import type { FC } from 'react';
 import { useEffect, useRef, useState } from 'react';
 import { match } from 'ts-pattern';
+import { format } from 'date-fns';
 
-const initialMessage =
-  'Please tell me welcome and introduce the top 3 news headlines of the day, then ask me which headlines I would like to dive into more. Do not acknowledge that you received this request. Start your response with: Welcome to Chatter, a daily news podcast';
+const today = format(new Date(), 'LLLL d, yyyy');
+
+// Initial message is necessary because the assistant can't speak first
+const initialMessage = `Start your response with: Welcome to Chatter, a daily news podcast. Then, please introduce the top 3 news headlines for today. The date is ${today}. After telling me the top headlines, ask me which headlines I would like to dive into more. Do not acknowledge that you received this request. `;
 
 export type ViewsProps = {
   transcriptMessages: Array<UserTranscriptMessage | AssistantTranscriptMessage>;
