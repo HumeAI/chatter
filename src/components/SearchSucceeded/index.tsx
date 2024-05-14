b43f6ea2fa87eb9b2ab3069b3e41b35ff2e6705f
Yini Shi <yini@hume.ai>
2024-05-14 16:07:58 -0400

    fix bg on web search messages

--- src/components/SearchSucceeded/index.tsx
+++ src/components/SearchSucceeded/index.tsx
@@ -12,10 +12,10 @@ export const SearchSucceeded: FC<SearchSucceededProps> = ({ message }) => {
   return (
     <AnimatePresence>
       <motion.div
-        className="-mt-2 mb-4 ml-auto flex w-fit items-center justify-center gap-2 bg-black text-neutral-200"
+        className="z-10 -mt-2 mb-4 ml-auto flex w-fit items-center justify-center gap-2 rounded-3xl bg-black px-2 py-1 text-neutral-200"
         data-tool-call-id={toolCallId}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        initial={{ opacity: 0, x: 0 }}
+        animate={{ opacity: 1, x: 10 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.1 }}
       >
