4ba331f4b31f11d807b10d652c9f7b2514792299
Yini Shi <yini@hume.ai>
2024-05-16 10:51:01 -0400

    remove screen size component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import { ScreenSize } from '@/components/ScreenSize';
 import { Voice } from '@/components/Voice';
 
 export default function Home() {
   return (
     <>
       <Voice />
-      {process.env.NODE_ENV !== 'production' && (
-        <div className="fixed bottom-2 left-2 bg-white">
-          <ScreenSize />
-        </div>
-      )}
     </>
   );
 }
