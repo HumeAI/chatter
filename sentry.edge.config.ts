0d6a4ce573a7bbac823ae41f60f70610b61a87cf
Yini Shi <yini@hume.ai>
2025-01-02 07:37:28 -0800

    move sentry DSN to env variable (#9)

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,10 +3,10 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://c25e0f77069805e271e110773a67be7a@o4505001794404352.ingest.us.sentry.io/4507246358364160",
+  dsn: process.env.SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
